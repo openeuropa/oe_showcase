f546aa5b2aedb515385627f56747196df1de20a5
Gilmar Lima <gilmar.lima@novacomm-europa.eu>
2021-12-22 15:03:16 +0200

    OEL-958: Add authentication trait.

---
+++ tests/src/Traits/AuthenticationTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_showcase\Traits;
+
+/**
+ * Provides methods for dealing with authentication in tests.
+ *
+ * @package Drupal\Tests\oe_showcase\Traits
+ */
+trait AuthenticationTrait {
+
+  /**
+   * Disables the forced login.
+   */
+  protected function disableForcedLogin(): void {
+    \Drupal::configFactory()->getEditable('cas.settings')->set('forced_login.enabled', FALSE)->save();
+  }
+
+  /**
+   * Enables the forced login.
+   */
+  protected function enableForcedLogin(): void {
+    \Drupal::configFactory()
+      ->getEditable('cas.settings')
+      ->set('forced_login.enabled', TRUE)
+      ->save();
+  }
+
+}
