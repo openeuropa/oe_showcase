823ee326d924fb5be6998f4520aa7af186a4621b
drishu <aszilagyi@live.com>
2022-01-25 13:02:54 +0200

    OEL-484: Update test.

--- modules/oe_showcase_user_profile/tests/src/ExistingSite/IntegrationTest.php
+++ modules/oe_showcase_user_profile/tests/src/ExistingSite/IntegrationTest.php
@@ -29,35 +29,50 @@ class IntegrationTest extends ShowcaseExistingSiteTestBase {
     $page->fillField('Username', 'example_user');
     $page->fillField('Password', '123456');
     $page->fillField('Confirm password', '123456');
-    $page->fillField('First Name', 'Exampleson');
-    $page->fillField('Last Name', 'McModel');
-    $page->fillField('Department', 'Development');
-    $page->fillField('Organisation', 'DIGIT');
+    $page->fillField('First name', 'Exampleson');
+    $page->fillField('Last name', 'McModel');
+    $page->fillField('Organization', 'DIGIT');
     $page->fillField('Bio', 'User description bio.');
     $page->fillField('Date', '1990-01-01');
     $page->fillField('Current position', 'Web Developer');
-    $page->fillField('Gender', 'male');
+    $page->selectFieldOption('Gender', 'male');
     $page->fillField('Country', 'BE');
     $page->fillField('Nationality', 'France');
     $page->fillField('Working Languages', 'French');
     $page->pressButton('Create new account');
 
     $user = user_load_by_name('example_user');
+    $user->get('allowed_options')->setValue([
+      'authenticated' => 'authenticated',
+    ]);
+    $user->get('visible_fields')->setValue([
+      'field_bio' => 'field_bio',
+      'field_city_country' => 'field_city_country',
+      'field_current_position' => 'field_current_position',
+      'field_date_of_birth' => 'field_date_of_birth',
+      'field_first_name' => 'field_first_name',
+      'field_gender' => 'field_gender',
+      'field_last_name' => 'field_last_name',
+      'field_nationality' => 'field_nationality',
+      'field_organization' => 'field_organization',
+      'field_profile_image' => 'field_profile_image',
+      'field_working_languages' => 'field_working_languages',
+    ]);
+    $user->save();
     $this->assertNotEmpty($user);
     $this->markEntityForCleanup($user);
     $this->drupalGet("user/{$user->id()}/edit");
 
-    $assert_session->fieldValueEquals('First Name', 'Exampleson');
-    $assert_session->fieldValueEquals('Last Name', 'McModel');
-    $assert_session->fieldValueEquals('Organisation', 'DIGIT');
-    $assert_session->fieldValueEquals('Department', 'Development');
+    $assert_session->fieldValueEquals('First name', 'Exampleson');
+    $assert_session->fieldValueEquals('Last name', 'McModel');
+    $assert_session->fieldValueEquals('Organization', 'DIGIT');
     $assert_session->fieldValueEquals('Bio', 'User description bio.');
-    $assert_session->fieldValueEquals('Gender', 'male (http://publications.europa.eu/resource/authority/human-sex/MALE)');
+    $assert_session->fieldValueEquals('Gender', 'http://publications.europa.eu/resource/authority/human-sex/MALE');
     $assert_session->fieldValueEquals('Country', 'BE');
     $assert_session->fieldValueEquals('Nationality', 'France (http://publications.europa.eu/resource/authority/country/FRA)');
     $assert_session->fieldValueEquals('Working Languages', 'French (http://publications.europa.eu/resource/authority/language/FRA)');
 
-    $this->drupalGet('users');
+    $this->drupalGet("user/{$user->id()}");
     $assert_session->pageTextContains('User description bio.');
     $assert_session->pageTextContains('male');
     $assert_session->pageTextContains('1990-01-01');
