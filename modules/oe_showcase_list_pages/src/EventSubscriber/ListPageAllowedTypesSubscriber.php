b233383680453719dc0e1c3ef67c74959ecb6455
drishu <aszilagyi@live.com>
2022-03-01 14:41:54 +0200

    OEL-1069: Add list_pages submodule.

---
+++ modules/oe_showcase_list_pages/src/EventSubscriber/ListPageAllowedTypesSubscriber.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types = 1);
+
+
+namespace Drupal\oe_showcase_list_pages\EventSubscriber;
+
+use Drupal\oe_list_pages\ListPageEvents;
+use Drupal\oe_list_pages\ListPageSourceAlterEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Subscribes to the list page allowed entity types event.
+ */
+class ListPageAllowedTypesSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ListPageEvents::ALTER_ENTITY_TYPES => 'alterEntityTypes',
+      ListPageEvents::ALTER_BUNDLES => 'alterBundles',
+    ];
+  }
+
+  /**
+   * Alters the entity types allowed by the List Page nodes.
+   *
+   * @param \Drupal\oe_list_pages\ListPageSourceAlterEvent $event
+   *   The event.
+   */
+  public function alterEntityTypes(ListPageSourceAlterEvent $event) {
+    $event->setEntityTypes(['node', 'media']);
+  }
+
+  /**
+   * Alters the bundles allowed by the List Page nodes.
+   *
+   * @param \Drupal\oe_list_pages\ListPageSourceAlterEvent $event
+   *   The event.
+   */
+  public function alterBundles(ListPageSourceAlterEvent $event) {
+    $allowed = [
+      'node' => [
+        'oe_event',
+        'oe_news',
+        'oe_showcase_page',
+        'oe_showcase_search_demo',
+      ],
+    ];
+
+    $entity_types = $event->getEntityTypes();
+    $entity_type = reset($entity_types);
+    if (isset($allowed[$entity_type])) {
+      $event->setBundles($entity_type, $allowed[$entity_type]);
+    }
+  }
+
+}
