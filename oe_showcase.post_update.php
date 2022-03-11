a2df12e2f52a6eb23b9388074665fe3041a120b3
Sergio Elvira Perez <sergioelviraperez@gmail.com>
2022-03-11 13:59:34 +0100

    OEL-1292: Post updated adapted to remove/update/create fields.

--- oe_showcase.post_update.php
+++ oe_showcase.post_update.php
@@ -13,21 +13,25 @@ use Drupal\oe_bootstrap_theme\ConfigImporter;
  * Update fields for starter news and events.
  */
 function oe_showcase_post_update_00001(array &$sandbox): void {
-  $entityTypeManager = \Drupal::service('entity_type.manager');
-  $types = [];
-  $contentTypes = $entityTypeManager->getStorage('node_type')->loadMultiple();
-  foreach ($contentTypes as $contentType) {
-    $types[$contentType->id()] = $contentType->label();
-    dump($types);
-  }
-  $configs = [
-    'block.block.openeuropa_content_language_switcher.yml',
-    'field.field.node.oe_sc_event.body.yml',
-    'field.field.node.oe_sc_event.oe_summary.yml',
-    'field.field.node.oe_sc_news.body.yml',
-    'field.field.node.oe_sc_news.oe_summary.yml',
-    'user.role.editor.yml',
+  $fields_delete = [
+    'field.field.node.oe_event.body',
+    'field.field.node.oe_event.oe_summary',
+    'field.field.node.oe_news.body',
+    'field.field.node.oe_news.oe_summary',
   ];
+  // Remove fields.
+  foreach ($fields_delete as $field) {
+    \Drupal::configFactory()->getEditable($field)->delete();
+  }
 
+  $configs = [
+    'block.block.openeuropa_content_language_switcher',
+    'field.field.node.oe_sc_event.body',
+    'field.field.node.oe_sc_event.oe_summary',
+    'field.field.node.oe_sc_news.body',
+    'field.field.node.oe_sc_news.oe_summary',
+    'user.role.editor',
+  ];
+  //Import/create config.
   ConfigImporter::importMultiple('oe_showcase', '/config/post_updates/00001/', $configs, TRUE);
 }
