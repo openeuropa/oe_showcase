84378b88619cebc6fb87c72d8fb6000c839f1f49
escuriola <escuriola@gmail.com>
2022-04-19 19:29:11 +0200

    OEL-1453: Update blocks region.

---
+++ oe_showcase.post_update.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * OpenEuropa Showcase post updates.
+ */
+
+declare(strict_types=1);
+
+use Drupal\block\Entity\Block;
+
+/**
+ * Set the updated regions of the oe_whitelabel navigation blocks.
+ */
+function oe_showcase_post_update_10001(&$sandbox) {
+  $blocks = [
+    'oe_whitelabel_eulogin' => 'header_top',
+    'oe_whitelabel_language_switcher' => 'header_top',
+    'oe_whitelabel_local_actions' => 'highlighted',
+    'oe_whitelabel_local_tasks' => 'highlighted',
+    'oe_whitelabel_messages' => 'highlighted',
+    'oe_whitelabel_main_navigation' => 'navigation_left',
+    'oe_whitelabel_search_form' => 'navigation_right',
+  ];
+
+  foreach ($blocks as $block_id => $region) {
+    $block = Block::load($block_id);
+
+    if ($block->getTheme() === 'oe_whitelabel') {
+      $block->setRegion($region);
+      $block->save();
+    }
+  }
+}
