fd1c809f351e58f7f50297cbf0b5769273f8ed71
drishu <aszilagyi@live.com>
2022-05-11 18:55:34 +0300

    OEL-1498: Add UserTrait test.

---
+++ tests/src/Traits/UserTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_showcase\Traits;
+
+use Drupal\user\UserInterface;
+
+/**
+ * Provides user related methods to be used in tests.
+ */
+trait UserTrait {
+
+  /**
+   * Gets a user by username.
+   *
+   * @param string $username
+   *   The users machine name.
+   *
+   * @return \Drupal\user\UserInterface
+   *   The user.
+   */
+  protected function getUserByName(string $username): UserInterface {
+    $users = \Drupal::entityTypeManager()
+      ->getStorage('user')
+      ->loadByProperties([
+        'name' => $username,
+      ]);
+
+    if (!$users) {
+      throw new \Exception(sprintf('The user with the name %s does not exist.', $username));
+    }
+
+    return reset($users);
+  }
+
+}
