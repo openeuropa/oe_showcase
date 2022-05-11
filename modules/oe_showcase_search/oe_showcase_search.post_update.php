a0faf47d5e8aa8660b4411668cd6fa184e5ad556
escuriola <escuriola@gmail.com>
2022-05-11 15:22:03 +0200

    OEL-1317: Update block info. Adapt to schema and add files to post_update.

--- modules/oe_showcase_search/oe_showcase_search.post_update.php
+++ modules/oe_showcase_search/oe_showcase_search.post_update.php
@@ -8,6 +8,7 @@
 declare(strict_types = 1);
 
 use Drupal\block\Entity\Block;
+use Drupal\oe_bootstrap_theme\ConfigImporter;
 
 /**
  * Ensure search blocks are in the correct order.
@@ -23,4 +24,15 @@ function oe_showcase_search_post_update_00001(): void {
     $block->set('weight', $weight);
     $block->save();
   }
+}
+
+/**
+ * Set the updated values to the search form block.
+ */
+function oe_showcase_search_post_update_00002(&$sandbox) {
+  $configs = [
+    'block.block.oe_whitelabel_search_form',
+    'block.block.showcase_header_search_form',
+  ];
+  ConfigImporter::importMultiple('module', 'oe_showcase_search', '/config/post_updates/00001', $configs);
 }
