4001fecca267b244668ee076637378300eb7c9fa
drishu <aszilagyi@live.com>
2022-05-11 19:32:51 +0300

    OEL-1498: Improve post update function comment.

--- oe_showcase.post_update.php
+++ oe_showcase.post_update.php
@@ -47,7 +47,7 @@ function oe_showcase_post_update_00002(&$sandbox) {
 }
 
 /**
- * Import the Manage users role.
+ * Enable roleassig, Manage users role and update settings.
  */
 function oe_showcase_post_update_00003(&$sandbox) {
   \Drupal::service('module_installer')->install(['roleassign']);
