97b69294ffd790c16b1cc0993d60170d6602f9ae
Francesco Sardara <francesco@tdgwebservices.com>
2022-07-07 21:30:44 +0200

    OEL-1727: Add test coverage to asser the order of post_update hooks.

---
+++ tests/modules/z_module/z_module.post_update.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * EWCMS Updates post updates.
+ */
+
+declare(strict_types = 1);
+
+/**
+ * Empty post update method.
+ *
+ * Because of its name this update would go to the end of the list of post
+ * update hooks that need to be executed.
+ */
+function z_module_post_update_00001(array &$sandbox) {
+}
