0fcff461cc49921157a6045da9dd4c4222430bce
Francesco Sardara <francesco@tdgwebservices.com>
2022-09-17 00:55:49 +0200

    OEL-2006: Add coverage for diacritics, pagination and increase number of letters covered.

--- modules/oe_showcase_glossary/tests/src/ExistingSite/GlossaryTest.php
+++ modules/oe_showcase_glossary/tests/src/ExistingSite/GlossaryTest.php
@@ -22,33 +22,7 @@ class GlossaryTest extends ShowcaseExistingSiteTestBase {
    * Tests the glossary view.
    */
   public function testGlossaryView(): void {
-    $glossary_vocabulary = Vocabulary::load('glossary');
-
-    $terms_by_letter = [];
-    for ($i = 0; $i < 50; $i++) {
-      $term = $this->createTerm($glossary_vocabulary, [
-        'langcode' => 'en',
-      ]);
-      $letter = mb_strtolower(substr($term->label(), 0, 1));
-      $terms_by_letter += [$letter => []];
-      $terms_by_letter[$letter][] = $term;
-    }
-
-    $terms_by_number = [];
-    for ($i = 0; $i < 10; $i++) {
-      // Make sure there's one entry with the number 0.
-      $starting_number = $i ? rand(0, 9) : 0;
-
-      $terms_by_number += [$starting_number => []];
-      $terms_by_number[$starting_number][] = $this->createTerm($glossary_vocabulary, [
-        'name' => $starting_number . $this->randomMachineName(),
-        'langcode' => 'en',
-      ]);
-    }
-
-    // Sort the terms alphabetically.
-    $this->sortTerms($terms_by_letter);
-    $this->sortTerms($terms_by_number);
+    $all_terms = $this->generateTerms();
 
     $this->drupalGet('/glossary');
     $assert_session = $this->assertSession();
@@ -68,12 +42,6 @@ class GlossaryTest extends ShowcaseExistingSiteTestBase {
       'links' => [],
     ];
 
-    // The characters in the summary are alphabetically sorted, with numbers
-    // last.
-    ksort($terms_by_letter);
-    ksort($terms_by_number);
-    $all_terms = $terms_by_letter + $terms_by_number;
-
     // Prepare all the expected links in the summary.
     foreach ($all_terms as $character => $terms) {
       $expected_summary['links'][] = [
@@ -92,8 +60,6 @@ class GlossaryTest extends ShowcaseExistingSiteTestBase {
     $pagination_assert = new PaginationPatternAssert();
     $pagination_assert->assertPattern($expected_summary, $summary->getOuterHtml());
 
-    $active_letter = key($terms_by_letter);
-    $this->assertViewResults($terms_by_letter[$active_letter], $active_letter);
 
     $previous_index = $index = 0;
     foreach ($all_terms as $character => $terms) {
@@ -120,15 +86,53 @@ class GlossaryTest extends ShowcaseExistingSiteTestBase {
    *   The expected character that will be shown in the title.
    */
   protected function assertViewResults(array $expected_terms, string $expected_character): void {
-    $this->assertSession()->elementTextEquals(
+    $assert_session = $this->assertSession();
+    $assert_session->elementTextEquals(
       'css',
       '.glossary-view > .glossary-view__results > h2.bcl-heading',
       sprintf('Starting with "%s" (%s)', mb_strtoupper($expected_character), count($expected_terms))
     );
 
+    $pager_selector = '.glossary-view > .glossary-view__results > nav[role="navigation"]';
+    if (count($expected_terms) < 21) {
+      $this->assertViewsResultPage($expected_terms);
+      $assert_session->elementNotExists('css', $pager_selector);
+      return;
+    }
+
     // Results are paged, 20 elements by page.
     $pages = array_chunk($expected_terms, 20);
-    foreach ($pages as $terms) {
+
+    $expected_pagination = [
+      'variant' => 'default',
+      'alignment' => 'center',
+      'links' => [],
+    ];
+    for ($i = 0; $i < count($pages); $i++) {
+      $expected_pagination['links'][] = [
+        'url' => '?sort_by=az&items_per_page=20&page=' . $i,
+        'label' => (string) ($i + 1),
+        'active' => $i === 0,
+      ];
+    }
+    $expected_pagination['links'][] = [
+      'url' => '?sort_by=az&items_per_page=20&page=1',
+      'label' => 'Next',
+    ];
+    $expected_pagination['links'][] = [
+      'url' => '?sort_by=az&items_per_page=20&page=' . count($pages) - 1,
+      'icon' => 'chevron-double-right',
+    ];
+
+    $pagination = $assert_session->elementExists('css', $pager_selector);
+    (new PaginationPatternAssert())->assertPattern($expected_pagination, $pagination->getOuterHtml());
+
+    foreach ($pages as $index => $terms) {
+      // Navigate to the next page if needed.
+      if ($index > 0) {
+        $assert_session->elementExists('named_exact', ['link', $index + 1], $pagination)->click();
+      }
+
       $this->assertViewsResultPage($terms);
     }
   }
@@ -167,6 +171,85 @@ class GlossaryTest extends ShowcaseExistingSiteTestBase {
   }
 
   /**
+   * Generates glossary taxonomy terms.
+   *
+   * @return array|array[]
+   *   A multidimensional array of terms, grouped by starting label character.
+   */
+  protected function generateTerms(): array {
+    $glossary_vocabulary = Vocabulary::load('glossary');
+
+    $initials = [];
+    $letter_pool = array_merge(range(65, 90), range(97, 122));
+    for ($i = 0; $i < 25; $i++) {
+      // Pick a random letter.
+      $index = array_rand($letter_pool);
+      // Add the letter with a random amount of terms to create for it.
+      $initials[chr($letter_pool[$index])] = rand(1, 5);
+      // Remove the "used" letter from the pool.
+      array_splice($letter_pool, $index, 1);
+    }
+
+    // To test that the summary is generated only using characters without
+    // diacritics, we make sure the letter î replaces the i.
+    unset($initials['i']);
+    unset($initials['I']);
+    $initials['î'] = 1;
+    // Always add the character c and with ç.
+    $initials['c'] = rand(1, 5);
+    $initials['ç'] = rand(1, 5);
+    // Add a special character.
+    $initials['Æ'] = rand(1, 5);
+    // Add one entry with more than 20 terms in it, to trigger paging.
+    $initials[array_rand($initials)] = 41;
+
+    $terms_by_letter = [];
+    foreach ($initials as $character => $num_terms) {
+      $terms = [];
+      for ($i = 0; $i < $num_terms; $i++) {
+        $terms[] = $this->createTerm($glossary_vocabulary, [
+          'name' => $character . $this->randomMachineName(),
+          'langcode' => 'en',
+        ]);
+      }
+
+      // Merge the terms starting with the same letter, ignoring the case.
+      $lowercase = mb_strtolower((string) $character);
+      $terms_by_letter[$lowercase] = array_merge($terms_by_letter[$lowercase] ?? [], $terms);
+    }
+
+    // Diacritics will be ignored by MySQL, so we merge the entries of c and ç.
+    $terms_by_letter['c'] = array_merge($terms_by_letter['c'], $terms_by_letter['ç']);
+    unset($terms_by_letter['ç']);
+    // And we replace the î with i.
+    $terms_by_letter['i'] = $terms_by_letter['î'];
+    unset($terms_by_letter['î']);
+
+    $terms_by_number = [];
+    for ($i = 0; $i < 10; $i++) {
+      // Make sure there's one entry with the number 0.
+      $starting_number = $i ? rand(0, 9) : 0;
+
+      $terms_by_number += [$starting_number => []];
+      $terms_by_number[$starting_number][] = $this->createTerm($glossary_vocabulary, [
+        'name' => $starting_number . $this->randomMachineName(),
+        'langcode' => 'en',
+      ]);
+    }
+
+    // Sort the terms alphabetically.
+    $this->sortTerms($terms_by_letter, 'letter');
+    $this->sortTerms($terms_by_number, 'number');
+
+    // The characters in the summary are alphabetically sorted, with numbers
+    // last.
+    ksort($terms_by_letter);
+    ksort($terms_by_number);
+
+    return $terms_by_letter + $terms_by_number;
+  }
+
+  /**
    * Sorts terms alphabetically.
    *
    * @param array $terms
@@ -174,11 +257,13 @@ class GlossaryTest extends ShowcaseExistingSiteTestBase {
    *   first level and taxonomy terms in the second level.
    */
   protected function sortTerms(array &$terms): void {
-    $sort_by_label = function (TermInterface $a, TermInterface $b): int {
-      return strnatcasecmp($a->label(), $b->label());
+    $transliteration = \Drupal::transliteration();
+
+    $sort_by_label = function (TermInterface $a, TermInterface $b) use ($transliteration): int {
+      return strcasecmp($transliteration->removeDiacritics($a->label()), $transliteration->removeDiacritics($b->label()));
     };
     array_walk($terms, function (array &$terms) use ($sort_by_label) {
-      uasort($terms, $sort_by_label);
+      usort($terms, $sort_by_label);
     });
   }
 
