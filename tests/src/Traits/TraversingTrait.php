09e707ec56f47c39f724ee5b27d2297a3cd40535
drishu <aszilagyi@live.com>
2022-10-18 11:51:29 +0300

    OEL-1990: Add method to find parent by attribute.

--- tests/src/Traits/TraversingTrait.php
+++ tests/src/Traits/TraversingTrait.php
@@ -30,4 +30,35 @@ trait TraversingTrait {
     return $options;
   }
 
+  /**
+   * Searches for the parent of an element by attribute, limited by depth.
+   *
+   * @param \Behat\Mink\Element\NodeElement $element
+   *   The element to start from.
+   * @param string $attribute
+   *   The parent attribute for comparison.
+   * @param string $value
+   *   The parent attribute value to compare to.
+   * @param int $depth
+   *   The maximum depth to limit to.
+   *
+   * @return \Behat\Mink\Element\NodeElement|void
+   *   The parent element if found.
+   */
+  protected function getParentByAttribute(NodeElement $element, string $attribute, string $value, int $depth = 5) {
+    $parent = $element->getParent();
+
+    // Limit the depth to prevent infinite loop.
+    for ($i = 0; $i < $depth; $i++) {
+      if (!$parent instanceof NodeElement) {
+        break;
+      }
+      if (strpos($parent->getAttribute($attribute), $value) !== FALSE) {
+        return $parent;
+      }
+
+      $parent = $parent->getParent();
+    }
+  }
+
 }
