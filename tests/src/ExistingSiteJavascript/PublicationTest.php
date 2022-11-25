e947a07f0f975afe173f6d2866681adf27b743b3
Francesco Sardara <francesco@tdgwebservices.com>
2022-11-25 23:43:56 +0100

    OEL-1807: Test the publication content type creation.

---
+++ tests/src/ExistingSiteJavascript/PublicationTest.php
@@ -0,0 +1,190 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_showcase\ExistingSiteJavascript;
+
+use Drupal\node\NodeInterface;
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Tests\oe_showcase\Traits\EntityBrowserTrait;
+use Drupal\Tests\oe_showcase\Traits\MediaCreationTrait;
+use Drupal\Tests\oe_showcase\Traits\TraversingTrait;
+use Drupal\Tests\oe_showcase\Traits\WysiwygTrait;
+use Drupal\Tests\oe_whitelabel\PatternAssertions\ContentBannerAssert;
+
+/**
+ * Tests the publication content type.
+ */
+class PublicationTest extends ShowcaseExistingSiteJavascriptTestBase {
+
+  use EntityBrowserTrait;
+  use MediaCreationTrait;
+  use TraversingTrait;
+  use WysiwygTrait;
+
+  /**
+   * Tests the creation of publication nodes.
+   *
+   * Covers the changes in the form and view modes introduced in oe_showcase.
+   */
+  public function testCreation(): void {
+    $this->markEntityTypeForCleanup('node');
+
+    $vocabulary = Vocabulary::load('publication_type');
+    $this->createTerm($vocabulary, ['name' => 'Type 1']);
+    $this->createTerm($vocabulary, ['name' => 'Type 2']);
+    $this->createPerson('John', 'Red');
+    $this->createPerson('Bob', 'Purple');
+    $media = $this->createTestMedia();
+
+    $user = $this->createUser([]);
+    $user->addRole('editor');
+    $user->save();
+    $this->drupalLogin($user);
+
+    $this->drupalGet('node/add/oe_sc_publication');
+    $assert_session = $this->assertSession();
+    $assert_session->fieldExists('Title')->setValue('Test publication');
+
+    // Check that the publication type field is present but do not select a
+    // value to check that it's not mandatory.
+    $this->assertEquals([
+      '- None -',
+      'Type 1',
+      'Type 2',
+    ], array_values($this->getSelectOptions($assert_session->fieldExists('Publication type'))));
+
+    // Assert the media browser for the thumbnail field.
+    $thumbnail_fieldset = $assert_session->elementExists('css', '[data-drupal-selector="edit-oe-featured-media-wrapper"]');
+    $assert_session->buttonExists('Select media', $thumbnail_fieldset)->press();
+    $assert_session->assertWaitOnAjaxRequest();
+    $this->getSession()->switchToIFrame('entity_browser_iframe_images');
+    $assert_session->linkExistsExact('Media library');
+    $assert_session->linkExistsExact('Search in AV Portal');
+
+    // Assert the exposed filters.
+    $assert_session->fieldExists('Filter by name');
+    $assert_session->fieldExists('Language');
+    $this->assertEquals([
+      'All' => '- Any -',
+      'av_portal_photo' => 'AV Portal Photo',
+      'image' => 'Image',
+    ], $this->getSelectOptions($assert_session->selectExists('Media type')));
+
+    // Make sure this entity browser shows only the expected media bundles.
+    $expected_media_bundles = [
+      'av_portal_photo',
+      'av_portal_video',
+      'image',
+      'remote_video',
+    ];
+    foreach (array_diff(array_keys($media), $expected_media_bundles) as $unwanted_bundle) {
+      $assert_session->pageTextNotContains($media[$unwanted_bundle]->label());
+    }
+
+    $this->getMediaBrowserTileByMediaName('Euro with miniature figurines');
+    $this->getMediaBrowserTileByMediaName('Image title')->click();
+    $assert_session->buttonExists('Select media')->press();
+    $this->getSession()->switchToIFrame();
+    $assert_session->assertWaitOnAjaxRequest();
+    $assert_session->pageTextContains('Image title');
+
+    $assert_session->fieldExists('Reference code')->setValue('I-9876987012');
+    // @todo Assert formats.
+    $assert_session->fieldExists('Short description')->setValue('Short description text.');
+    $this->enterTextInWysiwyg('Long description', 'Long description text.');
+    // @todo Setting dates in JS tests is not as straightforward as it seems.
+    // @see oe_content/tests/src/Behat/DateFieldContext.php
+    // Assert the media browser for the document field.
+    $document_fieldset = $assert_session->elementExists('css', '[data-drupal-selector="edit-oe-sc-publication-document-wrapper"]');
+    $assert_session->buttonExists('Select media', $document_fieldset)->press();
+    $assert_session->assertWaitOnAjaxRequest();
+    $this->getSession()->switchToIFrame('entity_browser_iframe_documents');
+    $assert_session->linkExistsExact('Media library');
+    $assert_session->linkExistsExact('Create new document');
+
+    // Assert the exposed filters.
+    $assert_session->fieldExists('Filter by name');
+    $assert_session->fieldExists('Language');
+    $assert_session->fieldNotExists('Media type');
+
+    // Make sure this entity browser shows documents.
+    $expected_media_bundles = ['document'];
+    foreach (array_diff(array_keys($media), $expected_media_bundles) as $unwanted_bundle) {
+      $assert_session->pageTextNotContains($media[$unwanted_bundle]->label());
+    }
+
+    // Assert that the "Create new document" tab allows to create a document
+    // media.
+    $this->getSession()->getPage()->findLink('Create new document')->click();
+    $assert_session->fieldExists('Name');
+    $assert_session->fieldExists('File Type');
+    $assert_session->buttonExists('Create and select document');
+
+    // Go back to the media library and select the test document.
+    $this->getSession()->getPage()->findLink('Media library')->click();
+    $this->getMediaBrowserTileByMediaName('Document title')->click();
+    $assert_session->buttonExists('Select media')->press();
+    $this->getSession()->switchToIFrame();
+    $assert_session->assertWaitOnAjaxRequest();
+    $assert_session->pageTextContains('Document title');
+
+    $assert_session->fieldExists('Authors')->setValue('John Red');
+    $assert_session->buttonExists('Add another item')->press();
+    $assert_session->assertWaitOnAjaxRequest();
+    $assert_session->fieldExists('oe_sc_publication_authors[1][target_id]')->setValue('Bob Purple');
+
+    $assert_session->buttonExists('Save')->press();
+    $assert_session->pageTextContains('Publication Test publication has been created.');
+
+    // Since the rendering of the content banner view mode has been changed,
+    // test that everything is rendered correctly when no publication type is
+    // selected.
+    $content_banner_assert = new ContentBannerAssert();
+    $expected_content_banner = [
+      'title' => 'Test publication',
+      // @todo Add date test.
+      'description' => 'Short description text.',
+      'badges' => [],
+      'image' => [
+        'alt' => 'Alt text',
+        'src' => $media['image']->get('oe_media_image')->entity->getFilename(),
+      ],
+    ];
+    $content_banner_assert->assertPattern($expected_content_banner, $assert_session->elementExists('css', '.bcl-content-banner')->getOuterHtml());
+
+    // Edit again the publication and select a type.
+    $publication = $this->getNodeByTitle('Test publication');
+    $this->drupalGet($publication->toUrl('edit-form'));
+    $assert_session->fieldExists('Publication type')->selectOption('Type 1');
+    $assert_session->buttonExists('Save')->press();
+    $assert_session->pageTextContains('Publication Test publication has been updated.');
+
+    $expected_content_banner['badges'] = ['Type 1'];
+    $content_banner_assert->assertPattern($expected_content_banner, $assert_session->elementExists('css', '.bcl-content-banner')->getOuterHtml());
+  }
+
+  /**
+   * Creates a person node with default values.
+   *
+   * @param string $first_name
+   *   The first name value.
+   * @param string $last_name
+   *   The last name value.
+   *
+   * @return \Drupal\node\NodeInterface
+   *   The person node.
+   */
+  protected function createPerson(string $first_name, string $last_name): NodeInterface {
+    return $this->createNode([
+      'type' => 'oe_sc_person',
+      'oe_sc_person_first_name' => $first_name,
+      'oe_sc_person_last_name' => $last_name,
+      'oe_sc_person_country' => 'IT',
+      'oe_sc_person_occupation' => $this->randomString(),
+      'oe_sc_person_position' => $this->randomString(),
+      'status' => 1,
+    ]);
+  }
+
+}
---
+++ tests/src/Traits/WysiwygTrait.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_showcase\Traits;
+
+use Behat\Mink\Element\NodeElement;
+
+/**
+ * Helper methods for interacting with WYSIWYG editors.
+ */
+trait WysiwygTrait {
+
+  /**
+   * Checks whether a WYSIWYG editor with the given field label is present.
+   *
+   * @param string $field
+   *   The label of the field to which the WYSIWYG editor is attached.
+   *
+   * @return bool
+   *   TRUE if the editor is present, FALSE otherwise.
+   */
+  protected function hasWysiwyg($field) {
+    try {
+      $this->getWysiwyg($field);
+      return TRUE;
+    }
+    // Only catch the specific exception that is thrown when the WYSIWYG editor
+    // is not present, let all other exceptions pass through.
+    catch (\Exception $e) {
+      return FALSE;
+    }
+  }
+
+  /**
+   * Presses the given WYSIWYG button.
+   *
+   * @param string $field
+   *   The field label of the field to which the WYSIWYG editor is attached. For
+   *   example 'Body'.
+   * @param string $button
+   *   The title of the button to click.
+   */
+  protected function pressWysiwygButton($field, $button): void {
+    $wysiwyg = $this->getWysiwyg($field);
+    $button_elements = $this->getSession()->getDriver()->find($wysiwyg->getXpath() . '//a[@title="' . $button . '"]');
+    if (empty($button_elements)) {
+      throw new \Exception("Could not find the '$button' button.");
+    }
+    if (count($button_elements) > 1) {
+      throw new \Exception("Multiple '$button' buttons found in the editor.");
+    }
+    $button = reset($button_elements);
+    $button->click();
+  }
+
+  /**
+   * Enters the given text in the textarea of the specified WYSIWYG editor.
+   *
+   * If there is any text existing it will be replaced.
+   *
+   * @param string $field
+   *   The field label of the field to which the WYSIWYG editor is attached. For
+   *   example 'Body'.
+   * @param string $text
+   *   The text to enter in the textarea.
+   */
+  protected function setWysiwygText($field, $text): void {
+    $wysiwyg = $this->getWysiwyg($field);
+    $textarea_elements = $this->getSession()->getDriver()->find($wysiwyg->getXpath() . '//textarea');
+    if (empty($textarea_elements)) {
+      throw new \Exception("Could not find the textarea for the '$field' field.");
+    }
+    if (count($textarea_elements) > 1) {
+      throw new \Exception("Multiple textareas found for '$field'.");
+    }
+    $textarea = reset($textarea_elements);
+    $textarea->setValue($text);
+  }
+
+  /**
+   * Returns the WYSIWYG editor that is associated with the given field label.
+   *
+   * This is hardcoded on the CKE editor which is included with Drupal core.
+   *
+   * @param string $field
+   *   The label of the field to which the WYSIWYG editor is attached.
+   *
+   * @return \Behat\Mink\Element\NodeElement
+   *   The WYSIWYG editor.
+   */
+  protected function getWysiwyg($field): NodeElement {
+    $driver = $this->getSession()->getDriver();
+    $label_elements = $driver->find('//label[text()="' . $field . '"]');
+    if (empty($label_elements)) {
+      throw new \Exception("Could not find the '$field' field label.");
+    }
+    if (count($label_elements) > 1) {
+      throw new \Exception("Multiple '$field' labels found in the page.");
+    }
+    $wysiwyg_id = 'cke_' . $label_elements[0]->getAttribute('for');
+    $wysiwyg_elements = $driver->find('//div[@id="' . $wysiwyg_id . '"]');
+    if (empty($wysiwyg_elements)) {
+      throw new \Exception("Could not find the '$field' wysiwyg editor.");
+    }
+    if (count($wysiwyg_elements) > 1) {
+      throw new \Exception("Multiple '$field' wysiwyg editors found in the page.");
+    }
+    return reset($wysiwyg_elements);
+  }
+
+  /**
+   * Enters the given text in the given WYSIWYG editor.
+   *
+   * @param string $label
+   *   The label of the field containing the WYSIWYG editor.
+   * @param string $text
+   *   The text to enter in the WYSIWYG editor.
+   */
+  protected function enterTextInWysiwyg(string $label, string $text): void {
+    $this->pressWysiwygButton($label, 'Source');
+    $this->setWysiwygText($label, $text);
+    // Make sure we switch back to normal view and let javascript to
+    // execute filters on the text and validate the html.
+    $this->pressWysiwygButton($label, 'Source');
+  }
+
+}
