7780c723fca09f441c17764415431d178017edff
AaronGilMartinez <aaron.gil.martinez@nttdata.com>
2023-11-24 12:03:55 +0100

    OEL-2541: Add function parameter.

--- test.sh
+++
@@ -1,8 +0,0 @@
-git stash
-git checkout 1.x
-docker-compose exec web ./vendor/bin/run drupal:site-install
-git checkout OEL-2541
-git stash apply
-docker-compose exec web drush updb -y
-docker-compose exec web drush cr
-docker-compose exec web ./vendor/bin/phpunit --testdox  modules/oe_showcase_search/
