d11f4f7c0ed827263087832112c34fff0d8c288c
AaronGilMartinez <aaron.gil.martinez@nttdata.com>
2023-11-30 11:53:37 +0100

    OEL-2635: Missing function parameter.

--- modules/oe_showcase_list_pages/oe_showcase_list_pages.post_update.php
+++ modules/oe_showcase_list_pages/oe_showcase_list_pages.post_update.php
@@ -12,7 +12,7 @@ use Drupal\oe_bootstrap_theme\ConfigImporter;
 /**
  * Add alias pattern for list pages.
  */
-function oe_showcase_list_pages_post_update_00001(): void {
+function oe_showcase_list_pages_post_update_00001(&$sandbox): void {
   ConfigImporter::importSingle(
     'module',
     'oe_showcase_list_pages',
