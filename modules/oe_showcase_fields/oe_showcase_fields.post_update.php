dd602cf45c8ecac3306d9474207ec1667528de14
Francesco Sardara <francesco@tdgwebservices.com>
2024-02-21 16:01:18 +0100

    OEL-2598: Fix comment.

--- modules/oe_showcase_fields/oe_showcase_fields.post_update.php
+++ modules/oe_showcase_fields/oe_showcase_fields.post_update.php
@@ -10,7 +10,7 @@ declare(strict_types = 1);
 use Drupal\oe_bootstrap_theme\ConfigImporter;
 
 /**
- * Add News type to News.
+ * Add the news type field to news node bundle.
  */
 function oe_showcase_fields_post_update_00001(): void {
   ConfigImporter::importMultiple('module', 'oe_showcase_fields', '/config/post_updates/00001_news_type', [
