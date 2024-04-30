3fbe925969de931fc96859a037e639ba6ea328c7
drishu <aszilagyi@live.com>
2024-04-30 22:20:15 +0300

    EPHEH-319: Fix update folder naming.

--- oe_showcase.post_update.php
+++ oe_showcase.post_update.php
@@ -905,7 +905,7 @@ function oe_showcase_post_update_00041(): void {
     'oe_color_scheme',
   ]);
 
-  ConfigImporter::importMultiple('profile', 'oe_showcase', '/config/post_updates/00039_color_scheme', [
+  ConfigImporter::importMultiple('profile', 'oe_showcase', '/config/post_updates/00040_color_scheme', [
     'field.storage.paragraph.field_color_scheme',
     'field.field.paragraph.oe_banner.field_color_scheme',
     'core.entity_form_display.paragraph.oe_banner.default',
