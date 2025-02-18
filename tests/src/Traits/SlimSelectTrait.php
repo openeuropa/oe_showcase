e68d4f88e04eacfd27ad81f1392d407d203ba31f
Piotr Smykaj <piotr.smykaj@gmail.com>
2025-02-18 17:14:47 +0100

    OEL-3504: Fix SlimSelectTrait.php for the Slim select v2.

--- tests/src/Traits/SlimSelectTrait.php
+++ tests/src/Traits/SlimSelectTrait.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\oe_showcase\Traits;
 
-use Behat\Mink\Element\ElementInterface;
 use Behat\Mink\Element\NodeElement;
-use WebDriver\Key;
 
 /**
  * Provides methods to work with slim_select form elements.
@@ -24,56 +22,46 @@ trait SlimSelectTrait {
    *   If old values should be kept.
    */
   protected function selectSlimOption(NodeElement $field, string $option, bool $multiple = FALSE): void {
-    $slim_select = $field->getParent()->find('css', 'div.ss-main');
+    $slim_select_main = $field->getParent()->find('css', 'div.ss-main');
+    $element_id = $field->find('css', '.form-select')->getAttribute('data-id');
+    $slim_select_content = $field->getParent()->getParent()->getParent()->find('css', 'div.ss-content[data-id="' . $element_id . '"]');
     if (!$multiple) {
       // Elements with no size are not interactable and can't be clicked, so we
-      // need to modify delete spans inside the select and make them use space.
+      // need to modify delete divs inside the select and make them use space.
       // By default this elements have an 'x' as content with no size.
       $script = <<<JS
         var closeElements = document.querySelectorAll(".ss-main .ss-values .ss-value-delete");
         for (var i = 0, max = closeElements.length; i < max; i++) {
-          closeElements[i].style.fontSize = '10px';
+          closeElements[i].style.height = '10px';
+          closeElements[i].style.width = '10px';
         }
       JS;
       $this->getSession()->executeScript($script);
-      $items = $slim_select->findAll('css', '.ss-values .ss-value-delete');
+      $items = $slim_select_main->findAll('css', '.ss-values .ss-value-delete');
       foreach ($items as $item) {
         $item->click();
       }
     }
 
-    // Helper function that checks if an element is visible. It differs from
-    // \Drupal\FunctionalJavascriptTests\JSWebAssert::waitForElementVisible()
-    // as it allows to pass a different container than page.
-    $page = $this->getSession()->getPage();
-
-    $fn_is_visible = static function (string $selector, $locator, ?ElementInterface $container = NULL) use ($page) {
-      $container ??= $page;
-      return $page->waitFor(10, static function () use ($selector, $locator, $container) {
-        $element = $container->find($selector, $locator);
-        if (!empty($element) && $element->isVisible()) {
-          return $element;
-        }
-        return NULL;
-      });
-    };
-
-    $slim_select->click();
-    // Wait for the dropdown to open.
-    $this->assertNotEmpty($fn_is_visible('css', '.ss-content.ss-open', $slim_select));
-    // Wait for the search input to be visible.
-    $slim_select_search = $fn_is_visible('css', 'div.ss-search > input', $slim_select);
-    $this->assertNotEmpty($slim_select_search);
+    $slim_select_main->click();
+    // Slim Select has minor delay in opening and closing the dialog so add a
+    // small delay to ensure that tests do not break due to fast automated
+    // actions.
+    $this->getSession()->wait(1000);
+    // Asserts the dropdown to be open.
+    $this->assertTrue($slim_select_content->find('css', '.ss-content[class*="ss-open-"]')->isVisible());
+    // Asserts the search input to be visible.
+    $slim_select_search = $slim_select_content->find('css', 'div.ss-search > input');
+    $this->assertTrue($slim_select_search->isVisible());
     $slim_select_search->setValue($option);
-    // Wait for the option to be visible.
-    $option_element = $fn_is_visible('xpath', '//div[contains(@class, "ss-option") and text()="' . $option . '"]', $slim_select);
-    $this->assertNotEmpty($option_element);
+    $this->getSession()->wait(1000);
+    // Asserts the option to be in the search result.
+    $option_element = $slim_select_content->find('xpath', '//div[contains(@class, "ss-option") and text()="' . $option . '"]');
+    $this->assertTrue($option_element->isVisible());
     $option_element->click();
-    // Wait for the option to be added to the list.
-    $this->assertNotEmpty($fn_is_visible('xpath', '//span[@class = "ss-value-text" and text()="' . $option . '"]', $slim_select));
-    // Close the element.
-    $slim_select->keyDown(Key::ESCAPE);
-    $slim_select->keyUp(Key::ESCAPE);
+    $this->getSession()->wait(1000);
+    // Asserts the option to be added to the list.
+    $this->assertTrue($slim_select_main->find('xpath', '//div[@class = "ss-value-text" and text()="' . $option . '"]')->isVisible());
   }
 
 }
