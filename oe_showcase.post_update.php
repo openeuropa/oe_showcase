85d674f5a863feb03daa8a86506dc7e3b5fc04e7
drishu <aszilagyi@live.com>
2025-04-10 18:46:24 +0300

    OEL-3662: Add person form and view config.

--- oe_showcase.post_update.php
+++ oe_showcase.post_update.php
@@ -1123,16 +1123,18 @@ function oe_showcase_post_update_00048(): void {
 
   ConfigImporter::importMultiple('profile', 'oe_showcase', '/config/post_updates/00047_agenda', [
     'core.entity_form_display.node.oe_sc_event.default',
+    'core.entity_form_display.oe_person.person.default',
     'core.entity_view_display.node.oe_sc_event.default',
     'core.entity_view_display.node.oe_sc_event.full',
     'core.entity_view_display.node.oe_sc_event.oe_w_content_banner',
     'core.entity_view_display.node.oe_sc_event.teaser',
+    'core.entity_view_display.oe_person.person.default',
     'field.field.node.oe_sc_event.field_oelp_agenda',
-    'oe_content_sub_entity_person.oe_person_type.person',
     'field.field.oe_agenda_session.oe_default.oe_session_moderators',
     'field.field.oe_agenda_session.oe_default.oe_session_speakers',
     'field.field.oe_person.person.field_oelp_person_name',
     'field.storage.node.field_oelp_agenda',
     'field.storage.oe_person.field_oelp_person_name',
+    'oe_content_sub_entity_person.oe_person_type.person',
   ]);
 }
