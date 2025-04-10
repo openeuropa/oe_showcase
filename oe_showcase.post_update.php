c65e96de4a0265a22171df9a45ba8638dc201ef4
drishu <aszilagyi@live.com>
2025-04-10 16:18:39 +0300

    OEL-3662: Update fields.

--- oe_showcase.post_update.php
+++ oe_showcase.post_update.php
@@ -1120,4 +1120,19 @@ function oe_showcase_post_update_00048(): void {
     'oe_content_sub_entity_person',
     'oe_whitelabel_agenda',
   ]);
+
+  ConfigImporter::importMultiple('profile', 'oe_showcase', '/config/post_updates/00047_agenda', [
+    'core.entity_form_display.node.oe_sc_event.default',
+    'core.entity_view_display.node.oe_sc_event.default',
+    'core.entity_view_display.node.oe_sc_event.full',
+    'core.entity_view_display.node.oe_sc_event.oe_w_content_banner',
+    'core.entity_view_display.node.oe_sc_event.teaser',
+    'field.field.node.oe_sc_event.field_oelp_agenda',
+    'field.field.oe_agenda_session.oe_default.oe_session_moderators',
+    'field.field.oe_agenda_session.oe_default.oe_session_speakers',
+    'field.field.oe_person.person.field_oelp_person_name',
+    'field.storage.node.field_oelp_agenda',
+    'field.storage.oe_person.field_oelp_person_name',
+    'oe_content_sub_entity_person.oe_person_type.person',
+  ]);
 }
