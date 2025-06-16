f06c3cfbe5e13f572cf0a1df41367d3f918dd416
Piotr Smykaj <piotr.smykaj@gmail.com>
2025-06-16 17:59:50 +0200

    OEL-3911: Update oe_webtools, oe_media and oe_paragraphs.

--- oe_showcase.post_update.php
+++ oe_showcase.post_update.php
@@ -1229,3 +1229,30 @@ function oe_showcase_post_update_00049(): void {
   }
   $role->save();
 }
+
+/**
+ * Delete deprecated webtools_social_feed media type.
+ */
+function oe_showcase_post_update_00050(array &$sandbox) {
+  // Delete all media entities of bundle 'webtools_social_feed'.
+  $media_storage = \Drupal::entityTypeManager()->getStorage('media');
+  $medias = $media_storage->loadByProperties([
+    'bundle' => 'webtools_social_feed',
+  ]);
+  if (!empty($medias)) {
+    $media_storage->delete($medias);
+    \Drupal::logger('oe_showcase')->notice('Deleted @count webtools_social_feed media items.', [
+      '@count' => count($medias),
+    ]);
+  }
+
+  // Remove the media bundle configuration.
+  $config_name = 'media.type.webtools_social_feed';
+  $config = \Drupal::configFactory()->getEditable($config_name);
+  if (!$config->isNew()) {
+    $config->delete();
+    \Drupal::logger('oe_showcase')->notice('Removed media bundle configuration @bundle.', [
+      '@bundle' => 'webtools_social_feed',
+    ]);
+  }
+}
