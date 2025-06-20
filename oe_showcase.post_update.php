fa2c172405e0fb9726ef346c94d57ebd89dc3fcb
Andreas Hennings <andreas@dqxtech.net>
2025-06-20 02:13:15 +0200

    OEL-3433: Switch full_html to ckeditor5, even though it is disabled.
    Keep most of the tags in allowed_html, except those that are redundant with enabled plugins.

--- oe_showcase.post_update.php
+++ oe_showcase.post_update.php
@@ -1249,8 +1249,10 @@ function oe_showcase_post_update_00051(): void {
   \Drupal::service('module_installer')->uninstall(['ckeditor']);
 
   ConfigImporter::importMultiple('profile', 'oe_showcase', '/config/post_updates/00050_ckeditor5', [
+    'filter.format.full_html',
     'filter.format.rich_text',
     'filter.format.simple_rich_text',
+    'editor.editor.full_html',
     'editor.editor.rich_text',
     'editor.editor.simple_rich_text',
   ]);
