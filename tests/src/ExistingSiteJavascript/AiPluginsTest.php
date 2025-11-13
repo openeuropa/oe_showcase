7a9e10a3846e1cbcaf9133c3854c37ce70c0a2b2
Enrique Lacoma Pérez <enriquelacomaperez@gmail.com>
2025-11-13 12:41:27 +0000

    OEL-4316: Add test for editor widget.

--- tests/src/ExistingSiteJavascript/AiPluginsTest.php
+++ tests/src/ExistingSiteJavascript/AiPluginsTest.php
@@ -21,16 +21,22 @@ class AiPluginsTest extends ShowcaseExistingSiteJavascriptTestBase {
     $user->addRole('editor');
     $user->save();
     $this->drupalLogin($user);
+    $assert_session = $this->assertSession();
+    // Test editoria11y is enabled.
+    $this->drupalGet('home');
+    $assert_session->waitForElement('css', 'ed11y-element-panel');
+    $assert_session->elementExists('css', 'ed11y-element-panel');
+
     $this->drupalGet('node/add/oe_sc_event');
 
     // Check content suggestion plugins are available.
-    $assert_session = $this->assertSession();
     $assert_session->pageTextContains('Alter tone');
     $assert_session->pageTextContains('Evaluate Readability');
     $assert_session->pageTextContains('Suggest title');
     $assert_session->pageTextContains('Summarise text');
     $assert_session->pageTextContains('Suggest taxonomy tags');
     $assert_session->pageTextContains('AI Assistant');
+
   }
 
 }
