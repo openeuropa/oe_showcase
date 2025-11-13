ec8fcb8a79dfd8010734fe75e387ee02beef0a95
Enrique Lacoma Pérez <enriquelacomaperez@gmail.com>
2025-11-13 15:10:31 +0000

    OEL-4316: Add permissions to editor role.

--- tests/src/ExistingSiteJavascript/AiPluginsTest.php
+++ tests/src/ExistingSiteJavascript/AiPluginsTest.php
@@ -21,21 +21,19 @@ class AiPluginsTest extends ShowcaseExistingSiteJavascriptTestBase {
     $user->addRole('editor');
     $user->save();
     $this->drupalLogin($user);
-    $assert_session = $this->assertSession();
     // Test editoria11y is enabled.
     $this->drupalGet('home');
-    $assert_session->waitForElement('css', 'ed11y-element-panel');
-    $assert_session->elementExists('css', 'ed11y-element-panel');
-
-    $this->drupalGet('node/add/oe_sc_event');
+    $this->assertSession()->waitForElement('css', 'ed11y-element-panel');
+    $this->assertSession()->elementExists('css', 'ed11y-element-panel');
 
     // Check content suggestion plugins are available.
-    $assert_session->pageTextContains('Alter tone');
-    $assert_session->pageTextContains('Evaluate Readability');
-    $assert_session->pageTextContains('Suggest title');
-    $assert_session->pageTextContains('Summarise text');
-    $assert_session->pageTextContains('Suggest taxonomy tags');
-    $assert_session->pageTextContains('AI Assistant');
+    $this->drupalGet('node/add/oe_sc_event');
+    $this->assertSession()->pageTextContains('Alter tone');
+    $this->assertSession()->pageTextContains('Evaluate Readability');
+    $this->assertSession()->pageTextContains('Suggest title');
+    $this->assertSession()->pageTextContains('Summarise text');
+    $this->assertSession()->pageTextContains('Suggest taxonomy tags');
+    $this->assertSession()->pageTextContains('AI Assistant');
 
   }
 
