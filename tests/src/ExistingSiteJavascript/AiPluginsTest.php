c62d8899ede04a05ad02d03d5492ad844c6bc605
Enrique Lacoma Pérez <enriquelacomaperez@gmail.com>
2025-12-03 12:33:19 +0000

    OEL-4316: Update ai tests.

--- tests/src/ExistingSiteJavascript/AiPluginsTest.php
+++ tests/src/ExistingSiteJavascript/AiPluginsTest.php
@@ -17,16 +17,17 @@ class AiPluginsTest extends ShowcaseExistingSiteJavascriptTestBase {
    * Test AI content plugins.
    */
   public function testContentPlugins(): void {
+    // Test for editor role.
     $user = $this->createUser([]);
     $user->addRole('editor');
     $user->save();
     $this->drupalLogin($user);
-    // Test editoria11y is enabled.
+    // Test editoria11y is enabled for editor role.
     $this->drupalGet('home');
     $this->assertSession()->waitForElement('css', 'ed11y-element-panel');
     $this->assertSession()->elementExists('css', 'ed11y-element-panel');
 
-    // Check content suggestion plugins are available.
+    // Test content suggestion plugins are available for editor role.
     $this->drupalGet('node/add/oe_sc_event');
     $this->assertSession()->pageTextContains('Alter tone');
     $this->assertSession()->pageTextContains('Evaluate Readability');
@@ -35,6 +36,27 @@ class AiPluginsTest extends ShowcaseExistingSiteJavascriptTestBase {
     $this->assertSession()->pageTextContains('Suggest taxonomy tags');
     $this->assertSession()->pageTextContains('AI Assistant');
 
+    // Test editor rol has access to the reports.
+    $this->drupalGet('/admin/reports/editoria11y');
+    $page_title = $this->getSession()->getPage()->find('css', 'h1.page-title')->getText();
+    $this->assertEquals('Content Accessibility Issues', $page_title);
+
+    // Test for non editor roles.
+    $user->addRole('configure_page_feedback_form');
+    $user->addRole('manage_contact_forms');
+    $user->addRole('manage_menu_items');
+    $user->addRole('manage_site_specific_footer');
+    $user->removeRole('editor');
+    $user->save();
+    $this->drupalLogin($user);
+
+    // Test editoria11y is disabled for non editor roles.
+    $this->drupalGet('home');
+    $this->assertSession()->elementNotExists('css', 'ed11y-element-panel');
+
+    // Test editor rol has access to the reports.
+    $this->drupalGet('/admin/reports/editoria11y');
+    $this->assertSession()->pageTextContains('You are not authorized to access this page.');
   }
 
 }
