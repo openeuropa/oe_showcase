3e0f5900920ead3dd43881738cb5c6a13b5664d2
Aaron Gil Martinez <aarongilmartinez@hotmail.com>
2025-12-12 09:59:10 +0100

    Revert "Merge pull request #354 from openeuropa/OEL-4316"
    This reverts commit ab954832ea6fd79688e1f4912d9ef474f146b872, reversing
    changes made to 19801b9eee50041510ab31cdb231263af11512e6.

--- tests/src/ExistingSiteJavascript/AiPluginsTest.php
+++ tests/src/ExistingSiteJavascript/AiPluginsTest.php
@@ -17,46 +17,20 @@ class AiPluginsTest extends ShowcaseExistingSiteJavascriptTestBase {
    * Test AI content plugins.
    */
   public function testContentPlugins(): void {
-    // Test for editor role.
     $user = $this->createUser([]);
     $user->addRole('editor');
     $user->save();
     $this->drupalLogin($user);
-    // Test editoria11y is enabled for editor role.
-    $this->drupalGet('home');
-    $this->assertSession()->waitForElement('css', 'ed11y-element-panel');
-    $this->assertSession()->elementExists('css', 'ed11y-element-panel');
-
-    // Test content suggestion plugins are available for editor role.
     $this->drupalGet('node/add/oe_sc_event');
-    $this->assertSession()->pageTextContains('Alter tone');
-    $this->assertSession()->pageTextContains('Evaluate Readability');
-    $this->assertSession()->pageTextContains('Suggest title');
-    $this->assertSession()->pageTextContains('Summarise text');
-    $this->assertSession()->pageTextContains('Suggest taxonomy tags');
-    $this->assertSession()->pageTextContains('AI Assistant');
 
-    // Test editor rol has access to the reports.
-    $this->drupalGet('/admin/reports/editoria11y');
-    $page_title = $this->getSession()->getPage()->find('css', 'h1.page-title')->getText();
-    $this->assertEquals('Content Accessibility Issues', $page_title);
-
-    // Test for non editor roles.
-    $user->addRole('configure_page_feedback_form');
-    $user->addRole('manage_contact_forms');
-    $user->addRole('manage_menu_items');
-    $user->addRole('manage_site_specific_footer');
-    $user->removeRole('editor');
-    $user->save();
-    $this->drupalLogin($user);
-
-    // Test editoria11y is disabled for non editor roles.
-    $this->drupalGet('home');
-    $this->assertSession()->elementNotExists('css', 'ed11y-element-panel');
-
-    // Test editor rol has access to the reports.
-    $this->drupalGet('/admin/reports/editoria11y');
-    $this->assertSession()->pageTextContains('You are not authorized to access this page.');
+    // Check content suggestion plugins are available.
+    $assert_session = $this->assertSession();
+    $assert_session->pageTextContains('Alter tone');
+    $assert_session->pageTextContains('Evaluate Readability');
+    $assert_session->pageTextContains('Suggest title');
+    $assert_session->pageTextContains('Summarise text');
+    $assert_session->pageTextContains('Suggest taxonomy tags');
+    $assert_session->pageTextContains('AI Assistant');
   }
 
 }
